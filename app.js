02c93296dcc5d27f8e41eb3f4e2b1d7d08ac7569
Horus <alcides.martinez@ahinko.com>
2022-06-29 07:24:30 -0600

    add

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get("/Hello", async (req, res) => {
     res.send("Hello mdf");
 });
 
+app.get("/Hello", async (req, res) => {
+    res.send("Alcides");
+});
+
 app.listen(process.env.PORT || 3000, () => {
     console.log("http server listen in port: 3000");
 });
